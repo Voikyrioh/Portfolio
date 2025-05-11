8c6305411714bbb56767159e34a17c2d70dfdea4
Voikyrioh <voikyrioh@gmail.com>
2025-05-11 22:38:00 +0200

    (refactor): renamed app directory to game.

--- src/env/production/main.ts
+++ src/env/production/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import '../../web/styles/style.css'
 import App from '../../web/App.vue'
-import {GameBuilder, GameType} from "../../app/game-builder.ts";
+import {GameBuilder, GameType} from "../../game/game-builder.ts";
 
 createApp(App).mount('#app')
 new GameBuilder(GameType.Prod).game.init().catch(console.error);
