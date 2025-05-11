18dbfb8c31f3312cd259fc9d48d614a8515720fe
Voikyrioh <voikyrioh@gmail.com>
2025-05-11 16:21:36 +0200

    (feat): presentation is reworked, text will show as a dialog bubble

--- src/web/components/presentation.vue
+++ src/web/components/presentation.vue
@@ -1,38 +1,58 @@
 <script setup lang="ts">
-  import {onMounted, ref} from "vue";
-  import i18n from "../services/i18n.ts";
-  import file from "../../../public/presentation.json?url";
-  import type { Presentation } from "../models/presentation.ts";
+import {onMounted, ref, watch} from "vue";
+import file from "../../../public/presentation.json?url";
+import type { Presentation } from "../models/presentation.ts";
+import I18n from "../services/i18n.ts";
 
   const text = ref('');
   onMounted(async () => {
-    text.value = (await i18n.getDocument<Presentation>(file)).text;
+    text.value = (await I18n.getDocument<Presentation>(file)).text;
   });
+  watch(I18n.languageRef, async () => { text.value = (await I18n.getDocument<Presentation>(file)).text; })
 </script>
 
 <template>
-  <p>{{text}}</p>
+  <div class="presentation">
+    <p>{{text}}</p>
+  </div>
 </template>
 
 <style scoped>
-  p {
-    color: var(--text-primary);
-    font-size: 1.2rem;
+  .presentation {
     max-width: 80%;
     padding: 16px;
-    margin: 0 auto;
-    text-align: center;
-  }
-
-  @media screen and (min-width: 900px) {
+    margin: 32px auto;
+    min-height: 500px;
+    p::after {
+      content: "";
+      display: block;
+      position: absolute;
+      background-color: var(--bg-default);
+      border-left: 1px solid;
+      border-bottom: 1px solid;
+      width: 32px;
+      height: 32px;
+      bottom: 0;
+      left: 0;
+      transform:  matrix(0.5, -0.5, 0, 1, -9, 9);
+    }
     p {
+      color: var(--text-primary);
+      font-size: 1.2rem;
+      padding: 16px;
+      text-align: center;
+      border-radius: 16px;
+      border: 1px solid;
+      position: relative;
+    }
+
+    @media screen and (min-width: 900px) {
       max-width: 60%;
     }
-  }
 
-  @media screen and (min-width: 1200px) {
-    p {
+    @media screen and (min-width: 1200px) {
       max-width: 33%;
     }
   }
+
 </style>
